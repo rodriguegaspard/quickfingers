5abc7a6974480a807d4da4a98b8691188832d95b
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-08-31 19:15:19 +0800

    Fixed a few bugs that caused the random word to be the same in a loop or not being fetched

--- main.c
+++ main.c
@@ -4,19 +4,18 @@
 #include <string.h>
 #include <ncurses.h>
 
-const char* getRandomWord(char *filename){
+char* getRandomWord(char *filename){
 	char buffer[4096];
-	FILE *file = fopen(filename, "r");
-	int lineCount, randomLine, lineNumber = 0;
-
-	srand(time(0));
+	FILE *file;
+	int lineCount, randomLine, lineNumber;
 
 	file = fopen(filename, "r");
 	if (file == NULL) {
-		printf("An error has occured. %s could not be accessed or does not exists.", filename);
+		printf("An error has occured. %s could not be accessed or does not exists.\n", filename);
 		return NULL;
 	}
 
+	lineCount = 0;
 	while (fgets(buffer, sizeof(buffer), file) != NULL) {
 		lineCount++;
 	}
@@ -25,27 +24,35 @@ const char* getRandomWord(char *filename){
 	randomLine = rand() % lineCount + 1;
 
 	file = fopen(filename, "r");
+	lineNumber = 0;
 	while (fgets(buffer, sizeof(buffer), file) != NULL) {
 		lineNumber++;
 		if (lineNumber == randomLine){
 			char *randomWord = malloc(sizeof(buffer));
 			strcpy(randomWord, buffer);
 			if (randomWord == NULL){
-				printf("Failed to allocate memory.");
+				printf("Failed to allocate memory.\n");
 				return NULL;
 			}
 			fclose(file);
+
 			return randomWord;
 		}
 	}
 
 	fclose(file);
-	printf("Failed to fetch a non-empty word.");
+	printf("Failed to fetch a non-empty word.\n");
 	return NULL;
 }
 
 int main()
 {
-	printf(getRandomWord("words_alpha.txt"));
+	srand(time(NULL));
+
+	for(int i=0 ; i<100 ; i++){
+		char *randomWord = getRandomWord("words_alpha.txt");
+		printf(randomWord);
+		free(randomWord);
+	}
 	return 0;
 }
