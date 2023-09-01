37c52e4224d29b8d18f54f989d5af523d355f6a4
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-09-01 14:31:05 +0800

    Refactored getRandomWord to be more efficient and readable, also renamed variables in snake_case. Added a little ncurses window that displays a random word.

--- main.c
+++ main.c
@@ -4,55 +4,63 @@
 #include <string.h>
 #include <ncurses.h>
 
-char* getRandomWord(char *filename){
-	char buffer[4096];
+#define MAX_WORD_LENGTH 512
+
+void getRandomWord(const char *filename, char* random_word){
+	char line[MAX_WORD_LENGTH];
 	FILE *file;
-	int lineCount, randomLine, lineNumber;
+	int line_count, random_line, line_number;
 
 	file = fopen(filename, "r");
 	if (file == NULL) {
 		printf("An error has occured. %s could not be accessed or does not exists.\n", filename);
-		return NULL;
 	}
 
-	lineCount = 0;
-	while (fgets(buffer, sizeof(buffer), file) != NULL) {
-		lineCount++;
+	line_count = 0;
+	while (fgets(line, MAX_WORD_LENGTH, file) != NULL) {
+		line_count++;
 	}
-	fclose(file);
 
-	randomLine = rand() % lineCount + 1;
+	random_line = rand() % line_count + 1;
 
 	file = fopen(filename, "r");
-	lineNumber = 0;
-	while (fgets(buffer, sizeof(buffer), file) != NULL) {
-		lineNumber++;
-		if (lineNumber == randomLine){
-			char *randomWord = malloc(sizeof(buffer));
-			strcpy(randomWord, buffer);
-			if (randomWord == NULL){
-				printf("Failed to allocate memory.\n");
-				return NULL;
-			}
-			fclose(file);
+	line_number = 0;
+	rewind(file);
 
-			return randomWord;
+	while (fgets(line, MAX_WORD_LENGTH, file) != NULL) {
+		line_number++;
+		if (line_number == random_line){
+			fclose(file);
+			strcpy(random_word, line);
+			return;
 		}
 	}
 
 	fclose(file);
 	printf("Failed to fetch a non-empty word.\n");
-	return NULL;
+	return;
 }
 
 int main()
 {
+	// INITIALIZATION
 	srand(time(NULL));
+	char *WORD_FILE = "english.txt";
+	char word[MAX_WORD_LENGTH];
 
-	for(int i=0 ; i<100 ; i++){
-		char *randomWord = getRandomWord("words_alpha.txt");
-		printf(randomWord);
-		free(randomWord);
-	}
+	initscr();
+	raw();
+	noecho();
+
+	getRandomWord(WORD_FILE, word);
+
+	attron(A_BOLD);
+	printw("Here's a random word. %s\n", word);
+	attroff(A_BOLD);
+
+	refresh();
+	getch();
+	endwin();
+
 	return 0;
 }
